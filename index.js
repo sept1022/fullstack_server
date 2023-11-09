1dd697898cea4a1c6bca2473d8b525fa52d0a55a
kyuyeol.han <kyuyeol.han@hyundai.com>
2023-11-10 00:25:33 +0900

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const { response, request } = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        important: true,
+    },
+    {
+        id: 2,
+        content: 'Browser can execute only JavaScript',
+        important: false,
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        important: true,
+    },
+]
+
+app.get('/', (request, response) => {
+    response.send('hello world')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.send(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find((n) => n.id === id)
+    return note ? response.send(note) : response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter((note) => note.id !== id)
+    console.log(notes[0].id === id)
+    console.log(notes)
+    response.status(204).end()
+})
+
+const getMaxId = () => {
+    if (notes.length === 0) return 0
+    const maxId = Math.max(...notes.map((note) => note.id))
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content is missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: getMaxId(),
+    }
+
+    notes = notes.concat(note)
+    console.log(notes)
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
